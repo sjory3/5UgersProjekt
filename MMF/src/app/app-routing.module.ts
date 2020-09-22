d25f48d34466dc83da6ca8610f2b9ac0edc7ff3f
ShizukuKiri <kira.kira@live.dk>
2020-09-22 11:56:01 +0200

    Initial commit

--- MMF/src/app/app-routing.module.ts
+++ MMF/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ApplicationComponent } from './application/application.component'
+import { ArchiveComponent } from './archive/archive.component'
+import { FrontPageComponent } from './front-page/front-page.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: 'application', component: ApplicationComponent },
+{ path: 'archive', component: ArchiveComponent },
+{ path: 'front-page', component: FrontPageComponent },
+{ path: '', redirectTo: '/front-page', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MMF/src/app/app.module.ts
+++ MMF/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { ApplicationComponent } from './application/application.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    ArchiveComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
