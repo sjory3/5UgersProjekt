05dfd4ff1ee8f6ff4530860ed688be0c99e736f8
ShizukuKiri <kira.kira@live.dk>
2020-10-01 22:48:52 +0200

    gui update

--- MMF/src/app/app.component.ts
+++ MMF/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +7,4 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'MMF';
-
-  @HostListener('window:scroll', ['$event'])
-
-  onWindowScroll(e): void {
-    let element = document.querySelector('.navbar');
-
-    if (window.pageYOffset > element.clientHeight) {
-      element.classList.add('bg-light');
-      element.classList.add('navbar-light');
-      element.classList.remove('navbar-dark');
-      element.classList.remove('bg-transparent');
-    } else {
-      element.classList.add('bg-transparent');
-      element.classList.add('navbar-dark');
-      element.classList.remove('bg-light');
-      element.classList.remove('navbar-light');
-    }
-  }
 }
--- MMF/src/app/application/application.component.ts
+++ MMF/src/app/application/application.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.css']
 })
-export class ApplicationComponent implements OnInit {
+export class ApplicationComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    const element = document.getElementById('mainNav');
+
+    element.classList.add('navbar-light');
+    element.classList.remove('navbar-dark');
+
+    document.addEventListener('scroll', this.onWindowScroll, false);
   }
 
+  ngOnDestroy(): void {
+    const element = document.getElementById('mainNav');
+
+    element.classList.remove('navbar-light');
+    element.classList.add('navbar-dark');
+
+    document.removeEventListener('scroll', this.onWindowScroll, false);
+  }
+
+  onWindowScroll(): void {
+    const element = document.getElementById('mainNav');
+
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('bg-light');
+      element.classList.remove('bg-transparent');
+    } else {
+      element.classList.add('bg-transparent');
+      element.classList.remove('bg-light');
+    }
+  }
 }
